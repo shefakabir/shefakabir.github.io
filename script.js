f5748d488298777ef8e2db8820b2f8d30e732688
Aitihya Rahman <aitihyaxero@gmail.com>
2025-02-04 23:11:27 +0600

    Updated everything

--- script.js
+++ script.js
@@ -2,13 +2,11 @@ document.getElementById('toggle-button').addEventListener('click', function() {
   const profilePicture = document.querySelector('.profile-picture');
   const profileDescription = document.querySelector('.profile-info p');
   const socialLinks = document.querySelector('.social-links');
-  const bottomIcons = document.querySelector('.bottom-icons');
   const buttonIcon = document.querySelector('#toggle-button i');
 
   profilePicture.classList.toggle('hidden');
   profileDescription.classList.toggle('expanded');
   socialLinks.classList.toggle('visible');
-  bottomIcons.classList.toggle('visible');
 
   if (socialLinks.classList.contains('visible')) {
     buttonIcon.classList.remove('fa-plus'); // Remove plus icon
